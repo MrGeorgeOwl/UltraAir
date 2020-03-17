8bce26f72fb8e141d9ca3fe2063b97d54dd84026
George Parhomenko <per4ik35@gmail.com>
2020-03-17 19:29:25 +0300

    Create class translating FlightEntity to Flight

---
+++ src/java/Service/FlightMapper.java
@@ -0,0 +1,17 @@
+package Service;
+
+import DAO.FlightEntity;
+
+import java.util.Date;
+
+public class FlightMapper {
+
+    public Flight mapToFlight(FlightEntity flightEntity){
+        String fromPlace = flightEntity.getFromPlace();
+        String toPlace = flightEntity.getToPlace();
+        Date departureDate = flightEntity.getDepartureDate();
+        int passengersAmount = flightEntity.getPassengersAmount();
+
+        return new Flight(fromPlace, toPlace, departureDate, passengersAmount);
+    }
+}
