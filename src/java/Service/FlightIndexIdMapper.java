2ead4e96cebf1085c3a821dbbd358bc7cb4d815e
George Parhomenko <per4ik35@gmail.com>
2020-03-19 00:09:36 +0300

    Create class for mapping index to id of flights

---
+++ src/java/Service/FlightIndexIdMapper.java
@@ -0,0 +1,22 @@
+package Service;
+
+import DAO.FlightEntity;
+import DAO.FlightRepository;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class FlightIndexIdMapper {
+    private ArrayList<Integer> indexToIdMap = new ArrayList<>();
+
+    public FlightIndexIdMapper() throws ParseException, java.text.ParseException, IOException {
+        Hashtable<Integer, FlightEntity>  hashtable = new FlightRepository().getAll();
+        indexToIdMap.addAll(hashtable.keySet());
+    }
+
+    public Integer getIdByIndex(int index){
+        return indexToIdMap.get(index);
+    }
+}
