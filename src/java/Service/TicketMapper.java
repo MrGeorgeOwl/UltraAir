52e58c17d3a30cc7ff6881991ef21ae15c7298cb
George Parhomenko <per4ik35@gmail.com>
2020-03-19 00:10:46 +0300

    Add mapping from TicketDTO to TicketEntity

--- src/java/Service/TicketMapper.java
+++ src/java/Service/TicketMapper.java
@@ -1,5 +1,6 @@
 package Service;
 
+import DAO.TicketEntity;
 import Presentation.TicketDTO;
 import org.json.simple.parser.ParseException;
 
@@ -7,11 +8,23 @@ import java.io.IOException;
 
 public class TicketMapper {
     private FlightService flightService = new FlightService();
+    private FlightIndexIdMapper flightIndexIdMapper = new FlightIndexIdMapper();
 
     public TicketMapper() throws ParseException, java.text.ParseException, IOException {
     }
 
-    // TODO: Create method for mapping data from DTO to Entity
+    public TicketEntity mapToTicketEntity(TicketDTO ticketDTO) throws Exception {
+        Ticket ticket = mapToTicket(ticketDTO);
+
+        Integer flightId = flightIndexIdMapper.getIdByIndex(ticketDTO.flightOrder);
+        double price = ticket.getPrice();
+        boolean rightFirstSitting = ticket.isRightFirstSitting();
+        boolean rightFirstRegistration = ticket.isRightFirstRegistration();
+
+        return new TicketEntity(flightId, price, rightFirstSitting, rightFirstRegistration);
+
+    }
+
     public Ticket mapToTicket(TicketDTO ticketDTO) throws Exception {
         String name = ticketDTO.clientName;
         boolean haveLuggage = ticketDTO.clientHaveLuggage;
