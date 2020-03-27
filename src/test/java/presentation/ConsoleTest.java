1a55409046986be8f1e65f8df0b2f6ec14af36f2
Timofey <timohatimafeikq@gmail.com>
2020-03-27 12:34:36 +0300

    add test of problematic place

---
+++ src/test/java/presentation/ConsoleTest.java
@@ -0,0 +1,24 @@
+package presentation;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import service.services.TicketService;
+
+import java.util.ArrayList;
+
+class ConsoleTest {
+    public static Logger log = LogManager.getLogger(ConsoleTest.class.getName());
+
+    @Test
+    public void showTicketsTest() throws Exception {
+        TicketService tickets = new TicketService();
+        ArrayList<String> ticketsList = tickets.getUserTicketsStrings("normie");
+        StringBuilder result = new StringBuilder();
+        for (String ticket : ticketsList) {
+            result.append(ticket);
+        }
+        log.debug(result);
+        //assertEquals(result.toString(), "");
+    }
+}
