4ce7465a0c51c2213fc7781d0a4dce7810ba9e49
Ignatello <46032039+IgnZarudko@users.noreply.github.com>
2020-05-31 01:06:10 +0300

    add flight REST API (get all, get by id, add)

---
+++ src/main/java/by/epam/ultraair/api/SpringApp.java
@@ -0,0 +1,13 @@
+package by.epam.ultraair.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.ParseException;
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp.class, args);
+    }
+}
