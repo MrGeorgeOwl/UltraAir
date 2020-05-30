152d917d1f60da19766afb2e53bf7319b01d0ee1
George Parhomenko <parhomenkogeorge@gmail.com>
2020-05-30 23:11:38 +0300

    Add method for getting all users and for finding one by login

--- src/main/java/by/epam/ultraair/persistence/service/UserService.java
+++ src/main/java/by/epam/ultraair/persistence/service/UserService.java
@@ -5,6 +5,7 @@ import by.epam.ultraair.dao.interfaces.UserDAO;
 import by.epam.ultraair.persistence.domain.User;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class UserService {
@@ -21,5 +22,18 @@ public class UserService {
             throw new SQLException("There is no such user %s", login);
         }
         return user.isAdmin();
+    }
+
+    public List<User> getAll(){
+        return userDAO.getAll();
+    }
+
+    public User findUserByLogin(String login) throws SQLException{
+        Optional<User> userOptional = userDAO.get(login);
+        User user = userOptional.orElse(null);
+        if (user == null){
+            throw new SQLException("There is no such user %s", login);
+        }
+        return user;
     }
 }
