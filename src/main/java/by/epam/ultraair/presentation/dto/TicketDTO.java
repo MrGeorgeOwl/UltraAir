60db3ed5f3ab1594ae97e5174a544dd46a65b190
Timofey <timohatimafeikq@gmail.com>
2020-05-30 18:23:58 +0300

    Cut off redundnat luggage

--- src/main/java/by/epam/ultraair/presentation/dto/TicketDTO.java
+++ src/main/java/by/epam/ultraair/presentation/dto/TicketDTO.java
@@ -2,16 +2,12 @@ package by.epam.ultraair.presentation.dto;
 
 public class TicketDTO {
     public String clientName;
-    public boolean clientHaveLuggage;
-    public double luggageWeight;
     public Integer flightID;
     public boolean wantRightFirstSitting;
     public boolean wantRightFirstRegistration;
 
     public TicketDTO(){
         clientName = "";
-        clientHaveLuggage = false;
-        luggageWeight = 0;
         flightID = 0;
         wantRightFirstSitting = false;
         wantRightFirstRegistration = false;
@@ -21,7 +17,6 @@ public class TicketDTO {
     public String toString() {
         return "Ticket {\n" +
                 "clientName = " + clientName + ";\n" +
-                "clientHaveLuggage = " + ((clientHaveLuggage) ? "yes" : "no") + ";\n" +
                 "flightID = " + (flightID + 1) + ";\n" +
                 "wantRightFirstSitting = " + ((wantRightFirstSitting) ? "yes" : "no") + ";\n" +
                 "wantRightFirstRegistration = " + ((wantRightFirstRegistration) ? "yes" : "no") + ";\n" +
